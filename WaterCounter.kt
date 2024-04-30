624d6b89631e2dc88eb2cbcf5c6526bbb7864f54
DanyCaballero <dany.caballero.1997@gmail.com>
2024-04-30 13:02:19 -0600

    Hoist state

--- WaterCounter.kt
+++ WaterCounter.kt
@@ -8,18 +8,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+    ) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
         if (count > 0)
             Text(text = "You've had $count glasses.")
         Button(
-            onClick = { count++ },
+            onClick = { onIncrement() },
             Modifier.padding(top = 8.dp),
             enabled = count < 10
         ) {
